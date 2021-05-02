3c1addb258e5fbd65606abb4db312f475f3331a0
rossmoody <fiftyfivehis@gmail.com>
2021-05-02 13:53:04 -0700

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: 'rossmoody.com',
+    title: 'Ross Moody',
     description: `The portfolio site for Ross Moody.`,
     siteUrl: 'https://www.rossmoody.com',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ['G-08M9MV3L72'],
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
+      },
+    },
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
