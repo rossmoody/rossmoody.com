f93e510f2fd9fd6d37fa6d337b631fb6c5e6ac57
rossmoody <fiftyfivehis@gmail.com>
2021-05-07 11:21:22 -0700

    commit message

--- src/components/footnote/index.tsx
+++ src/components/footnote/index.tsx
@@ -3,7 +3,7 @@ import { Text, Tooltip } from '@chakra-ui/react'
 
 import footnotes from '../../data/footnotes.json'
 
-export const Footnote = ({ name }: { name: string }): JSX.Element => {
+export const Footnote = ({ name }: { name: string }) => {
   const item = footnotes.find((object) => object[name])
 
   if (!item) throw new Error('Problems...')
