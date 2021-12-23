bfe6c8d355c343d47cc0b2efe3670733b9f805db
rossmoody <fiftyfivehis@gmail.com>
2021-12-23 11:37:49 -0800

    Updates

--- src/components/LinkTooltip.tsx
+++ src/components/LinkTooltip.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Tooltip, Text, Link, Box } from '@chakra-ui/react'
+import { Tooltip, Text, Link } from '@chakra-ui/react'
 import links from '../data/links.json'
 
 interface TooltipLabelProps {
