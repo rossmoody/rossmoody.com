9cca0bf6c0b100a6bb5c280f9c029690829ff717
rossmoody <fiftyfivehis@gmail.com>
2021-12-25 15:17:16 -0800

    update

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Box, Divider } from '@chakra-ui/react'
+import { Divider } from '@chakra-ui/react'
 import { Hero } from './Hero'
 import { History } from './History'
 
 export const Main = () => (
-  <Box as="main">
+  <>
     <Hero />
     <Divider my="60px" variant="dashed" borderColor="divider" />
     <History />
-  </Box>
+  </>
 )
