b2b61786be2766c2b9efb530b900f321d04782f5
rossmoody <fiftyfivehis@gmail.com>
2021-12-25 14:06:21 -0800

    SSR

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { ChakraProvider, Box } from '@chakra-ui/react'
 import { Header } from './Header'
 import { Footer } from './Footer'
@@ -14,12 +14,14 @@ const getRandoTheme = () => {
 export const Layout: React.FC = ({ children }) => {
   const [theme, setTheme] = useState(getRandoTheme)
 
+  const themeMemo = useMemo(() => theme, [theme])
+
   const handleSetThemeState = () => {
     setTheme(getRandoTheme)
   }
 
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={themeMemo}>
       <Box px={8} bg="surface" pt="48px" pb="60px" minHeight="100vh">
         <Header themeToggle={handleSetThemeState} />
         <Box maxW="4xl" marginX="auto" as="main">
