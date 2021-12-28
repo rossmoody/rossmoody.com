43f1b16f89aea9380c605e8b06b1114625df4623
rossmoody <fiftyfivehis@gmail.com>
2021-12-27 17:46:22 -0800

    Google analytics

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,29 @@
 import remarkGfm from 'remark-gfm'
 import { remarkMdxCodeMeta } from 'remark-mdx-code-meta'
 
-export const reactStrictMode = true
-export const experimental = { esmExternals: true }
-export const pageExtensions = ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js']
-export const webpack = (config, options) => {
-  config.module.rules.push({
-    test: /\.mdx?$/,
-    use: [
-      options.defaultLoaders.babel,
-      {
-        loader: '@mdx-js/loader',
-        options: {
-          providerImportSource: '@mdx-js/react',
-          remarkPlugins: [remarkGfm, remarkMdxCodeMeta],
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { esmExternals: true },
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          options: {
+            providerImportSource: '@mdx-js/react',
+            remarkPlugins: [remarkGfm, remarkMdxCodeMeta],
+          },
         },
-      },
-    ],
-  })
-  return config
+      ],
+    })
+    return config
+  },
 }
+
+export default nextConfig
