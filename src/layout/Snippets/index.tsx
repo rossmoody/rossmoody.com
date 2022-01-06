3b1c7105601257dd764d213949309c832e1d6f2b
rossmoody <fiftyfivehis@gmail.com>
2022-01-06 11:40:11 -0800

    Responsive

--- src/layout/Snippets/index.tsx
+++ src/layout/Snippets/index.tsx
@@ -3,21 +3,25 @@ import { DashDivider, SnippetsPreview } from 'components'
 import { Layout } from 'layout'
 import { SnippetsPosts } from 'utils/getFrontMatter'
 
-export const SnippetsLayout = ({ posts }: SnippetsPosts) => (
-  <Layout>
-    <SlideFade in={true} offsetY="12px">
-      <Box as="h1" textStyle="h1">
-        Snippets
-      </Box>
-      <Text>
-        Helpful code snippets I find myself repeatedly referencing and using.
-      </Text>
-      <DashDivider my="8" />
-      <SimpleGrid columns={[1, 2, 3]} spacing="6">
-        {posts.map((post) => (
-          <SnippetsPreview {...post} key={post.slug} />
-        ))}
-      </SimpleGrid>
-    </SlideFade>
-  </Layout>
-)
+export const SnippetsLayout = ({ posts }: SnippetsPosts) => {
+  if (!posts) return
+
+  return (
+    <Layout>
+      <SlideFade in={true} offsetY="12px">
+        <Box as="h1" textStyle="h1">
+          Snippets
+        </Box>
+        <Text>
+          Helpful code snippets I find myself repeatedly referencing and using.
+        </Text>
+        <DashDivider my="8" />
+        <SimpleGrid columns={[1, 2, 3]} spacing="6">
+          {posts.map((post) => (
+            <SnippetsPreview {...post} key={post.slug} />
+          ))}
+        </SimpleGrid>
+      </SlideFade>
+    </Layout>
+  )
+}
