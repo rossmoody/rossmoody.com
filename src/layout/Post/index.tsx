d550bc8ef538fd9d935a1286cf4b8293b8710abd
rossmoody <fiftyfivehis@gmail.com>
2022-01-05 21:53:15 -0800

    Responsive

--- src/layout/Post/index.tsx
+++ src/layout/Post/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, useBreakpointValue } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import { PageHeader, Seo, TableOfContents } from 'components'
 import { Layout } from 'layout'
 import { MDXRemoteSerializeResult } from 'next-mdx-remote'
@@ -12,16 +12,17 @@ export type PostPageProps = {
 const PostLayout: React.FC<Pick<PostPageProps, 'frontMatter'>> = (props) => {
   const { children, frontMatter } = props
 
-  const showTOC = useBreakpointValue({
-    base: 'minmax(0, 1fr)',
-    md: 'minmax(0, 1fr) 200px',
-  })
-
   return (
     <Layout maxWidth="5xl">
       <Seo {...frontMatter} />
       <PageHeader {...frontMatter} />
-      <Grid templateColumns={showTOC} gap="12">
+      <Grid
+        templateColumns={{
+          base: 'minmax(0, 1fr)',
+          md: 'minmax(0, 1fr) 200px',
+        }}
+        gap="12"
+      >
         <GridItem id="page-content">{children}</GridItem>
         <GridItem>
           <TableOfContents />
