8c7db2d2dc9a4fc01099f8cdb5bfc3c78e0baa85
Ross Moody <fiftyfivehis@gmail.com>
2022-04-09 14:45:59 -0700

    Update History.tsx

--- src/layout/Home/History.tsx
+++ src/layout/Home/History.tsx
@@ -12,7 +12,7 @@ export const History = () => (
 				color="textProminent"
 		>
 				I started my <Note name="career" words="career"/> running a screenprinting{' '}
-				<Tip name="stationery">stationery company</Tip> where I printed all kinds of{' '}
+				<Tip name="stationery">stationery company</Tip> printing all kinds of{' '}
 				<Tip name="fun">fun products</Tip> out of a 1-bedroom apartment in{' '}
 				<Note name="nyc" words="NYC"/>. I worked as a{' '}
 				<Tip name="lettering">hand-lettering artist</Tip>,{' '}
@@ -26,11 +26,11 @@ export const History = () => (
 				learning <Note name="js" words="JavaScript"/> while helping launch a
 				browser at <Tip name="brave">Brave</Tip> that was powered by{' '}
 				<Tip name="crypto">cryptocurrency</Tip>. From there I began focusing on
-				design systems and writing effective documentation to speed align design and engineering teams.
+				design systems and writing effective documentation to align design and engineering teams.
 				<br/>
 				<br/>
 				These days I split my time between being a Principal Design Technologist at{' '}
-				<Tip name="splunk">Splunk</Tip> and tinkering with dApp development. I like <Note name="french"
+				<Tip name="splunk">Splunk</Tip> and tinkering with app development. I like <Note name="french"
 				                                                                                  words="French Bulldogs"/>
 				, <Note name="cryptocurrency" words="cryptocurrency"/>, and my{' '}
 				<Tip name="aprilia">Aprilia</Tip> RS 660. If you let me too close to your
